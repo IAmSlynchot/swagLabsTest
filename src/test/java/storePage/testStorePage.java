cbb5706d67e69e5fcdaa66fd25a88fe3a1583f45
Sam Stone <slynch@DeepThought.local>
2019-02-13 18:01:11 -0800

    gitignore fix

--- src/test/java/storePage/testStorePage.java
+++ src/test/java/storePage/testStorePage.java
@@ -24,14 +24,44 @@ public class testStorePage {
     }
 
     @Test
-    public void login() throws InterruptedException {
+    public void login() {
         driver.get("https://www.saucedemo.com/");
-        By userNameField = new By.ById("user-name");
-        driver.findElement(userNameField).sendKeys("standard_user");
-        By passwordField = new By.ById("password");
-        driver.findElement(passwordField).sendKeys("secret_sauce");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.className("login-button")).click();
-        Thread.sleep(4000);
+        Assert.assertTrue(driver.findElement(By.className("product_label")).isDisplayed());
+    }
+
+    @Test
+    public void buyAFleece() {
+        // Navigate to main page and look at an item
+        driver.get("https://www.saucedemo.com/inventory.html");
+        driver.findElement(By.id("item_5_title_link")).click();
+        String itemName = driver.findElement(By.className("inventory_details_name")).getText();
+        Assert.assertEquals(itemName, "Sauce Labs Fleece Jacket");
+
+        // Add item to cart and go to cart page
+        driver.findElement(By.className("add-to-cart-button")).click();
+        driver.findElement(By.className("shopping_cart_link")).click();
+        Assert.assertTrue(driver.findElement(By.className("cart_item")).isDisplayed());
+        String cartItemName = driver.findElement(By.className("inventory_item_name")).getText();
+        Assert.assertEquals(cartItemName, "Sauce Labs Fleece Jacket");
+
+        // Begin checkout process and add personal info
+        driver.findElement(By.className("cart_checkout_link")).click();
+        driver.findElement(By.cssSelector("[data-test=\"firstName\"]")).sendKeys("Bruce");
+        driver.findElement(By.cssSelector("[data-test=\"lastName\"]")).sendKeys("Wayne");
+        driver.findElement(By.cssSelector("[data-test=\"postalCode\"]")).sendKeys("53540");
+        driver.findElement(By.className("cart_checkout_link")).click();
+        String overviewHeaderText = driver.findElement(By.className("subheader_label")).getText();
+        Assert.assertEquals(overviewHeaderText, "Checkout: Overview");
+
+        // Complete checkout process and verify complete
+        Assert.assertTrue(driver.findElement(By.className("cart_item")).isDisplayed());
+        String overviewItemName = driver.findElement(By.className("inventory_item_name")).getText();
+        Assert.assertEquals(overviewItemName, "Sauce Labs Fleece Jacket");
+        driver.findElement(By.className("cart_checkout_link")).click();
+        Assert.assertTrue(driver.findElement(By.className("complete-header")).isDisplayed());
     }
 
     @AfterClass
