79859c7f7e4d28b26e7cf5c040c3d39470290867
Sam Stone <slynch@DeepThought.local>
2019-02-13 18:53:08 -0800

    adding test for add and remove item

--- src/test/java/storePage/testStorePage.java
+++ src/test/java/storePage/testStorePage.java
@@ -1,6 +1,7 @@
 package storePage;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
@@ -9,7 +10,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.AfterClass;
 
 public class testStorePage {
-    public WebDriver driver;
+    private WebDriver driver;
 
     @BeforeClass
     public void beforeClass() {
@@ -17,25 +18,83 @@ public class testStorePage {
         driver = new ChromeDriver();
     }
 
+    private void loginToStore() {
+        driver.get("https://www.saucedemo.com/");
+        // Auth details should never be tracked like this in a real test, but they're already written right out on the site
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.className("login-button")).click();
+    }
+
+    private void resetAppData() {
+        try {
+            // Open menu, click "reset app data" to clear cart, close menu
+            driver.findElement(By.className("bm-burger-button")).click();
+            Thread.sleep(300); // pause for sidebar to open
+            driver.findElement(By.id("reset_sidebar_link")).click();
+            driver.findElement(By.className("bm-cross-button")).click();
+            Thread.sleep(300); // pause for sidebar to close
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+    private boolean isElementPresent(By locator) {
+        try {
+            driver.findElement(locator);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
     @Test
-    public void openStorePage() {
+    public void testOpenStorePage() {
         driver.get("https://www.saucedemo.com/");
         Assert.assertEquals(driver.getTitle(), "Swag Labs");
     }
 
     @Test
-    public void login() {
-        driver.get("https://www.saucedemo.com/");
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.className("login-button")).click();
+    public void testLogin() {
+        loginToStore();
         Assert.assertTrue(driver.findElement(By.className("product_label")).isDisplayed());
     }
 
     @Test
-    public void buyAFleece() {
+    public void testBadLogin() {
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("fake_user");
+        driver.findElement(By.id("password")).sendKeys("something_fake");
+        driver.findElement(By.className("login-button")).click();
+        String errorText = driver.findElement(By.cssSelector("[data-test=\"error\"]")).getText();
+        Assert.assertEquals(errorText, "Epic sadface: Username and password do not match any user in this service");
+    }
+
+    @Test
+    public void testAddAndRemoveItem() {
+        loginToStore();
+        resetAppData();
+        driver.findElement(By.id("item_0_title_link")).click();
+        String itemName = driver.findElement(By.className("inventory_details_name")).getText();
+        Assert.assertEquals(itemName, "Sauce Labs Bike Light");
+
+        // Add item to cart and go to cart page
+        driver.findElement(By.className("add-to-cart-button")).click();
+        driver.findElement(By.className("shopping_cart_link")).click();
+        Assert.assertTrue(driver.findElement(By.className("cart_item")).isDisplayed());
+        String cartItemName = driver.findElement(By.className("inventory_item_name")).getText();
+        Assert.assertEquals(cartItemName, "Sauce Labs Bike Light");
+
+        // Remove item from cart and ensure that it is gone
+        driver.findElement(By.className("remove-from-cart-button")).click();
+        Assert.assertFalse(isElementPresent(By.className("inventory_item_name")));
+    }
+
+    @Test
+    public void testBuyAFleece() {
         // Navigate to main page and look at an item
-        driver.get("https://www.saucedemo.com/inventory.html");
+        loginToStore();
+        resetAppData();
         driver.findElement(By.id("item_5_title_link")).click();
         String itemName = driver.findElement(By.className("inventory_details_name")).getText();
         Assert.assertEquals(itemName, "Sauce Labs Fleece Jacket");
